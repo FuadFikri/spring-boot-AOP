d61cac1417aeb2141cb7048994e0bb3d53ab011e
fikri <fuadfikri01@gmail.com>
2024-11-16 20:40:02 +0700

    init

---
+++ src/test/java/com/fikri/belajar_spring_aop/BelajarSpringAopApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fikri.belajar_spring_aop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+
+class BelajarSpringAopApplicationTests {
+
+
+}
